573183375298187f986641f3f27faf4af767b347
Wahyudi Septiawan <wahyu.d.septiawan@gmail.com>
2024-01-19 15:33:00 +0800

    Praktikum Membuat Project Flutter CLI Application

---
+++ bin/hsiflutter.dart
@@ -0,0 +1,5 @@
+import 'package:hsiflutter/hsiflutter.dart' as hsiflutter;
+
+void main(List<String> arguments) {
+  print('Hello world: ${hsiflutter.calculate()}!');
+}
---
+++ test/hsiflutter_test.dart
@@ -0,0 +1,8 @@
+import 'package:hsiflutter/hsiflutter.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
